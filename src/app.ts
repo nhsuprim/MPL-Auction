0d03c5ac96c0618c5dea1930f673113bb6d9512a
nhsuprim <nhsuprim24@gmail.com>
2024-10-01 22:38:45 +0600

    ready for production

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
     res.send({
-        Message: "Supreme health care server..",
+        Message: "Mirhazirbag Premier League server..",
     });
 });
 
