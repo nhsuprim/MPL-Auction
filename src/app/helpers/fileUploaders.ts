e7adf9d0fd2c678719a959f0a16dd7cef65aa1c8
A.K.M Nazmul Huda <nhuprim24@gmail.com>
2025-01-28 14:08:36 +0600

    update deploy

--- src/app/helpers/fileUploaders.ts
+++ src/app/helpers/fileUploaders.ts
@@ -2,18 +2,18 @@ import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 import { ICloudinaryRes, IFile } from "../interface/file";
 
-// Configuration for Cloudinary
+
 cloudinary.config({
     cloud_name: "dgak3ha1i",
     api_key: "172911419654983",
     api_secret: "8KCIwv6b63vh--oIHRbMjjfuejY",
 });
 
-// Use memory storage instead of disk storage
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Upload an image to Cloudinary
+
 const uploadToCloudinary = (file: IFile): Promise<ICloudinaryRes> => {
     return new Promise((resolve, reject) => {
         cloudinary.uploader
@@ -22,12 +22,12 @@ const uploadToCloudinary = (file: IFile): Promise<ICloudinaryRes> => {
                     return reject(error);
                 }
                 if (result) {
-                    resolve(result as unknown as ICloudinaryRes); // Type assertion
+                    resolve(result as unknown as ICloudinaryRes);
                 } else {
                     reject(new Error("Cloudinary upload failed."));
                 }
             })
-            .end(file.buffer); // Upload directly from memory buffer
+            .end(file.buffer); 
     });
 };
 
